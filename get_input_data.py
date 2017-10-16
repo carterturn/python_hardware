34518a1034339533a3a4c9cde7237b9198fe2332
Carter Turnbaugh <carterturn@tutanota.de>
2017-10-15 20:15:29 -0700

    Removed non-amalgamated scripts

--- get_input_data.py
+++
@@ -1,9 +0,0 @@
-from Xlib.display import Display
-from Xlib.ext import xinput
-
-display = Display()
-
-devices = display.xinput_query_device(0)._data['devices']
-
-for device in devices:
-    print(device['name'])
